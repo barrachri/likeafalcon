4437da94a0921ba3fc42f0ad225da2b19a8f9854
Christian Barra <barrachri@gmail.com>
2017-12-15 19:38:55 +0100

    Add app/models.py.

---
+++ app/models.py
@@ -0,0 +1,23 @@
+"""
+App models.
+"""
+
+from sqlalchemy import (
+    JSON,
+    Column,
+    DateTime,
+    Integer,
+    Table,
+)
+from sqlalchemy.sql import func
+
+from factory import metadata
+
+Event = Table(
+    'events',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('created_at', DateTime(timezone=True),
+           server_default=func.now()),
+    Column('data', JSON),
+)
