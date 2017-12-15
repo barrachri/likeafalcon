364c6212c2d503819f7c0b40e931ad75a2e42ece
Christian Barra <barrachri@gmail.com>
2017-12-15 21:06:35 +0100

    Add main.py and conftest.py.

---
+++ main.py
@@ -0,0 +1,72 @@
+from app import routes
+from aiohttp import web
+from rampante import streaming, scheduler
+import asyncio
+from config import Config as C
+from aiopg.sa import create_engine
+from psycopg2 import OperationalError
+import logging.config
+import logging
+
+logging.config.dictConfig(C.DEFAULT_LOGGING)
+log = logging.getLogger(__name__)
+
+
+def add_route(app, *args):
+    """
+    Add routes to app instance.
+    :app: instance of the app
+    """
+    for route in args:
+        app.router.add_route(route[0], route[1], route[2])
+
+
+async def start_db_pool(app):
+    """Create db connection."""
+    # Database engine is shared across the requests
+    try:
+        engine = await create_engine(
+            user=C.DB_USER,
+            password=C.DB_PASSWORD,
+            database=C.DB_NAME,
+            host=C.DB_HOST,
+            port=C.DB_PORT,
+        )
+    except OperationalError as err:
+        log.error("Are you sure that POSTGRESQL is working?")
+        raise err
+    app["db"] = engine
+
+
+async def stop_db_pool(app):
+    """Cancel db pool."""
+    if 'db' in app:
+        app['db'].close()
+        await app["db"].wait_closed()
+
+
+async def start_task_manager(app):
+    """Connect to the streams."""
+    await streaming.start(
+        server=C.STREAM_URI, client_name="service-01", service_group="service-likeafalcon", loop=app.loop)
+    app['task_manager'] = asyncio.ensure_future(
+        scheduler(queue_size=50, loop=app.loop, app=app))
+
+
+async def stop_task_manager(app):
+    """Cancel task manager."""
+    await streaming.stop()
+    if 'task_manager' in app:
+        app['task_manager'].cancel()
+        await app['task_manager']
+
+if __name__ == "__main__":
+    app = web.Application()
+    add_route(app, *routes)
+    # On-startup tasks
+    app.on_startup.append(start_task_manager)
+    app.on_startup.append(start_db_pool)
+    # Clean-up tasks
+    app.on_cleanup.append(stop_task_manager)
+    app.on_cleanup.append(stop_db_pool)
+    web.run_app(app)
---
+++ tests/conftest.py
@@ -0,0 +1,87 @@
+from aiohttp import web
+import pytest
+from app import routes
+
+import asyncio
+import logging
+import logging.config
+
+from aiohttp.test_utils import TestClient, TestServer
+from aiopg.sa import create_engine as aiopg_create_engine
+from psycopg2 import OperationalError
+from sqlalchemy import create_engine
+from sqlalchemy.schema import CreateTable
+from sqlalchemy_utils.functions import (
+    create_database,
+    database_exists,
+    drop_database,
+)
+from config import Config as C
+from main import (
+    add_route,
+    start_db_pool,
+    stop_db_pool,
+    start_task_manager,
+    stop_task_manager,
+)
+from app.models import Event
+
+
+@pytest.fixture(scope="session")
+def loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    yield loop
+
+    # Clean-up
+    loop.close()
+
+@pytest.fixture(scope="session", autouse=True)
+def test_client(loop):
+    clients = []
+
+    async def go(__param, *args, server_kwargs=None, **kwargs):
+
+        server_kwargs = server_kwargs or {}
+        server = TestServer(__param, loop=loop, **server_kwargs)
+        client = TestClient(server, loop=loop, **kwargs)
+
+        await client.start_server()
+        clients.append(client)
+        return client
+
+    yield go
+
+    async def finalize():
+        while clients:
+            await clients.pop().close()
+
+    loop.run_until_complete(finalize())
+
+
+@pytest.fixture(scope="session")
+def create_tables():
+    """Create all the tables needed for the test."""
+    DB_URI = f'postgresql://{C.DB_USER}:{C.DB_PASSWORD}@{C.DB_HOST}:{C.DB_PORT}/{C.DB_NAME}'
+    if database_exists(DB_URI):
+        drop_database(DB_URI)
+    create_database(DB_URI)
+    engine = create_engine(DB_URI)
+
+    conn = engine.connect()
+    models = [Event]
+    for model in models:
+        conn.execute(CreateTable(model).compile(engine).__str__())
+
+
+@pytest.fixture(scope="session")
+def cli(loop, test_client, create_tables):
+    app = web.Application()
+    add_route(app, *routes)
+    # On-startup tasks
+    app.on_startup.append(start_task_manager)
+    app.on_startup.append(start_db_pool)
+    # Clean-up tasks
+    app.on_cleanup.append(stop_task_manager)
+    app.on_cleanup.append(stop_db_pool)
+    return loop.run_until_complete(test_client(app))
